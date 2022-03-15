5e6fe86965adf38fccf2f88af888df3b42ebe210
George Matta <jojomedhat2004@gmail.com>
2022-03-15 15:23:23 -0700

    completely implemented the ResizeableArrayStack

--- ArrayStackTest.java
+++ ArrayStackTest.java
@@ -1,3 +1,62 @@
 public class ArrayStackTest {
 
+    // CONSTRUCTOR
+    // Check when capacity is 0
+    // Check when capacity isn't provided
+    // Check when capacity is between 1 and maximum
+    // Check when capacity is greater than the maximum
+
+    // PUSH
+    // Push an entry
+        // Check new num of entries (make sure it increased)
+        // Check stack
+    // Push more entries
+        // Check new num of entries (make sure it increased)
+        // Check stack
+    // Make sure we can't push more than maximum num of entries
+    // make sure we double capacity correctly
+
+    // POP
+        // Pop when stack is empty
+        // Pop when stack has items
+        // Pop when stack is full
+        // Check that we returned the correct item
+        // Check num of entries (make sure it decreased)
+
+    // PEEK
+        // peek when stack is empty
+        // Check num of entries (make sure it didn't change)
+        // check that we returned the correct item
+
+    // ISEMPTY
+        // check when stack is empty
+        // check when stack is not empty
+
+    // CLEAR
+        // clear when stack is empty
+        // clear when stack is full
+        // check number of entries (make sure it's 0)
+        // check stack contents (make sure it's empty)
+
+
+    // THIS IS JUST A TEMPORARY MAIN METHOD TO MAKE SURE I'M NOT BEING DUMB
+    public static void main(String[] args){
+        ResizeableArrayStack<String> ras = new ResizeableArrayStack<String>();
+
+        System.out.println(ras);
+        ras.push("hello");
+        System.out.println(ras);
+        ras.push("world");
+        System.out.println(ras);
+        ras.push("!");
+        System.out.println(ras);
+        System.out.println(ras.peek());
+        System.out.println(ras.pop());
+        System.out.println(ras);
+        System.out.println(ras.pop());
+        System.out.println(ras);
+        ras.clear();
+        System.out.println(ras);
+    }
+
 }
--- ResizeableArrayStack.java
+++ ResizeableArrayStack.java
@@ -1,3 +1,5 @@
+import java.util.EmptyStackException;
+
 /**
  * This is the ResizeableArrayStack class which implements the StackInterface interface.
  *
@@ -8,7 +10,7 @@
  * Each method is documented separately.
  *
  * @author George Matta
- * @version 1.0
+ * @version 1.2
  */
 public class ResizeableArrayStack<T> implements StackInterface<T>{
 
@@ -60,6 +62,16 @@ public class ResizeableArrayStack<T> implements StackInterface<T>{
     }
 
     /**
+     * A private method to create a generic-type array
+     * @param capacity The capacity of the array object
+     * @return The array object of T generics
+     */
+    @SuppressWarnings("unchecked")
+    private T[] createStack(int capacity){
+        return (T[]) new Object[capacity];
+    }
+
+    /**
      * A method to throw an error if the capacity of the bag is too large or too small
      * @param capacity The capacity we are checking
      */
@@ -69,20 +81,20 @@ public class ResizeableArrayStack<T> implements StackInterface<T>{
             errorMessage += "large";
             throw new IllegalStateException(errorMessage);
         }
-        if (capacity < 0){
+        if (capacity < 1){
             errorMessage += "small";
             throw new IllegalStateException(errorMessage);
         }
     }
 
     /**
-     * A private method to create a generic-type array
-     * @param capacity The capacity of the array object
-     * @return The array object of T generics
+     * A method to check if the number of entries is larger than the maximum capacity
+     * If so, we double the capacity
      */
-    @SuppressWarnings("unchecked")
-    private T[] createStack(int capacity){
-        return (T[]) new Object[capacity];
+    private void ensureCapacity(){
+        if(numOfEntries >= stack.length){
+            doubleCapacity();
+        }
     }
 
     /**
@@ -119,32 +131,91 @@ public class ResizeableArrayStack<T> implements StackInterface<T>{
         return newStack;
     }
 
+    /**
+     * A method to push a new entry into our stack. With this ADT, we add to the end of the array
+     * @param newEntry The generic object entry we are adding
+     */
     @Override
     public void push(T newEntry) {
         checkIntegrity();
+        ensureCapacity();
+
+        stack[numOfEntries] = newEntry;
+        numOfEntries++;
     }
 
+    /**
+     * A method to remove an entry from our stack. With this ADT, we remove from the end of the array.
+     * @return The generic object we popped from the stack
+     */
     @Override
     public T pop() {
         checkIntegrity();
-        return null;
+
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+
+        T popped = stack[numOfEntries - 1];
+        stack[numOfEntries - 1] = null;
+        numOfEntries--;
+
+        return popped;
     }
 
+    /**
+     * A method to return the top entry in our stack (without removing it)
+     * @return The generic object at the top of the stack
+     */
     @Override
     public T peek() {
         checkIntegrity();
-        return null;
+
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+
+        return stack[numOfEntries - 1];
     }
 
+    /**
+     * A method to check if our stack is empty. This just checks if the number of entries is less than 1.
+     * @return A boolean denoting whether or not the stack is empty
+     */
     @Override
     public boolean isEmpty() {
         checkIntegrity();
-        return false;
+        return numOfEntries < 1;
     }
 
+    /**
+     * A method to clear our entire stack. We just continuously remove the top entry while the stack is not empty.
+     */
     @Override
     public void clear() {
         checkIntegrity();
+
+        while(!isEmpty()){
+            stack[numOfEntries] = null;
+            numOfEntries--;
+        }
+    }
+
+    /**
+     * A private method to return a String representation of the array
+     */
+    public String toString(){
+        String result = "[";
+
+        for(int i = 0; i < numOfEntries; i++){
+            result += stack[i];
+            if(i != numOfEntries - 1){
+                result += ", ";
+            }
+        }
+        result += "]";
+
+        return result;
     }
 
 
